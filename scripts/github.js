7000127172bda1ecf89809b8e1434ec52d7a7f47
loghinalexandru <loghinalexandru61@gmail.com>
2019-02-08 17:37:07 +0200

    HomePage done

--- scripts/github.js
+++ scripts/github.js
@@ -1,14 +1,32 @@
+let caption = document.getElementById("githubCaption");
+
+function isInViewport(element) {
+	var rect = element.getBoundingClientRect();
+	var html = document.documentElement;
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || html.clientHeight) &&
+		rect.right <= (window.innerWidth || html.clientWidth)
+	);
+}
+
 function getGithubData(uri){
 	fetch(uri)
 		.then(response => {
 			return response.json();
 		})
 		.then(data =>{
-			console.log(data.public_repos);
+			let counter = document.getElementsByClassName("odometer")[0];
+			counter.innerHTML = data.public_repos
 		})
 }
 
-window.onload = () => {
-	getGithubData('https://api.github.com/users/loghinalexandru');
+function renderData(){
+	if(isInViewport(caption)){
+		getGithubData('https://api.github.com/users/loghinalexandru');
+	}
 }
+
+window.addEventListener("scroll" , renderData);
 
